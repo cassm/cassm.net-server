510031501c13c43e31c12da577cd3b079f2f5e60
Cass May <cass@cassm.net>
2022-04-05 17:25:11 +0100

    send form submissions via nodemailer

--- routes/formdata.js
+++ routes/formdata.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const nodemailer = require('../src/nodemailer/nodemailer');
 const router = express.Router();
 
 router.post('/', function(req, res, next) {
-  console.log(req)
-  res.send(200);
+  const submission = req.body;
+
+  res.sendStatus(200);
+
+  console.log(submission);
+  const transporter = nodemailer.transporter.getInstance();
+  transporter.sendMail({
+    from: `"${submission.name}" <${submission.email}>`,
+    to: "cass@cassm.net",
+    subject: 'Contact form submission from cassm.net',
+    text: submission.message
+  }).then(info => console.log(info));
 });
 
 module.exports = router;
