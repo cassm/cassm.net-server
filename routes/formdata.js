9adbd0e60c97ce53daa36a0840a534ddd3a1c9ad
Cass May <cass@cassm.net>
2022-04-05 17:37:13 +0100

    send copy of contact form to sender if requested

--- routes/formdata.js
+++ routes/formdata.js
@@ -15,6 +15,23 @@ router.post('/', function(req, res, next) {
     subject: 'Contact form submission from cassm.net',
     text: submission.message
   }).then(info => console.log(info));
+
+  if (submission.sendcopy) {
+    transporter.sendMail({
+      from: '"Cassandra May" <cass@cassm.net>',
+      to: submission.email,
+      subject: 'Your cassm.net contact form submission',
+      text: `Hi ${submission.name},
+
+      Thanks for getting in contact! I'll get back to you shortly.
+
+      -Cass
+
+      ------------------------------------------------
+
+      ${submission.message}`
+    }).then(info => console.log(info));
+  }
 });
 
 module.exports = router;
